b560b36d4599e8d46f7471b584b3692065603179
Sinval Neto <70284242+sinvalbsneto@users.noreply.github.com>
2021-06-19 14:24:57 -0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function changeTopLeft(value) {
+  let preview = document.getElementById("preview");
+
+  preview.style.borderTopLeftRadius = value + '%';
+}
+
+function changeTopRight(value) {
+  let preview = document.getElementById("preview");
+
+  preview.style.borderTopRightRadius = value + '%';
+}
+
+function changeBottomLeft(value) {
+  let preview = document.getElementById("preview");
+
+  preview.style.borderBottomLeftRadius = value + '%';
+}
+
+function changeBottomRight(value) {
+  let preview = document.getElementById("preview");
+
+  preview.style.borderBottomRightRadius = value + '%';
+}
